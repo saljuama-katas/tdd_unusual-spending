7a291719c3e894a1b8d5118b6bedcad9c1a82755
Salvador Juan Martinez <saljuama@gmail.com>
2020-02-18 00:25:31 +0100

    refactor/redesign initial collaborations
    - split responsibilities
    - simplify logic in our main entry point

--- src/main/scala/spending/TriggerUnusualSpendingEmail.scala
+++ src/main/scala/spending/TriggerUnusualSpendingEmail.scala
@@ -1,28 +1,12 @@
 package spending
 
-class TriggerUnusualSpendingEmail(val paymentFetcher: FetchesUserPaymentsByMonth,
-                                  val emailSender: UserEmailsWrapper) {
+class TriggerUnusualSpendingEmail(val unusualExpensesAnalyzer: UnusualExpensesAnalyzer,
+                                  val unusualExpensesNotifier: UnusualExpensesNotifier) {
 
   def trigger(userId: Long): Unit = {
-    val currentMonthExpenses = paymentFetcher.fetch(userId, 2020, 2)
-    val lastMonthExpenses = paymentFetcher.fetch(userId, 2020, 1)
-
-    val lastMonth = lastMonthExpenses.map { _.price }.sum
-    val currentMonth = currentMonthExpenses.map { _.price }.sum
-    val ratio =
-      if (lastMonth > BigDecimal(0.0) && currentMonth > BigDecimal(0.0))
-        lastMonth / currentMonth
-      else
-        BigDecimal(0.0)
-
-    if (ratio >= 1.5)
-      emailSender.sendEmail(1L, "Unusual spending", "Hello card user!")
+    val unusualExpenses = unusualExpensesAnalyzer.analyzeExpenses(userId)
+    if (unusualExpenses.nonEmpty)
+      unusualExpensesNotifier.notifyUser(1L, unusualExpenses)
   }
 
-}
-
-trait UserEmailsWrapper {
-  def sendEmail(userId: Long, subject: String, body: String): Unit = {
-    EmailsUser.email(userId, subject, body)
-  }
 }
--- src/test/scala/spending/TriggerUnusualSpendingEmailTest.scala
+++ src/test/scala/spending/TriggerUnusualSpendingEmailTest.scala
@@ -8,35 +8,28 @@ import org.scalatest.wordspec.AnyWordSpec
 class TriggerUnusualSpendingEmailTest extends AnyWordSpec with Matchers with MockFactory {
 
   trait Setup {
-    val fetchesUserPaymentsByMonth: FetchesUserPaymentsByMonth = mock[FetchesUserPaymentsByMonth]
-    val mailerWrapper: UserEmailsWrapper = mock[UserEmailsWrapper]
-    val unusualSpendingEmail = new TriggerUnusualSpendingEmail(fetchesUserPaymentsByMonth, mailerWrapper)
+    val unusualExpensesAnalyzer: UnusualExpensesAnalyzer = mock[UnusualExpensesAnalyzer]
+    val unusualExpensesNotifier: UnusualExpensesNotifier = mock[UnusualExpensesNotifier]
+    val unusualSpendingEmail = new TriggerUnusualSpendingEmail(unusualExpensesAnalyzer, unusualExpensesNotifier)
   }
 
-  "Trigger Unusual Spending Email" when {
+  "Triggering the process for a user" must {
+    val userId: Long = 1L
 
-    "triggering the process of unusual spending" must {
-      "not send an email" when {
-        "the user has no payments in the current and past month" in new Setup {
-          (fetchesUserPaymentsByMonth.fetch _).expects(1L, 2020, 2).returning(Seq())
-          (fetchesUserPaymentsByMonth.fetch _).expects(1L, 2020, 1).returning(Seq())
+    "NOT send an user notification when the user does not have unusual expenses" in new Setup {
+      (unusualExpensesAnalyzer.analyzeExpenses _).expects(userId).returning(Seq())
+      (unusualExpensesNotifier.notifyUser _).expects(*, *).never
 
-          unusualSpendingEmail.trigger(1L)
-        }
-      }
+      unusualSpendingEmail.trigger(userId)
+    }
 
-      "send an email" when {
-        "expenses in the current month are >= 50% compared to last month" in new Setup {
-          (fetchesUserPaymentsByMonth.fetch _).expects(1L, 2020, 2).returning(Seq(Payment(100.22, "yo", Category.entertainment)))
-          (fetchesUserPaymentsByMonth.fetch _).expects(1L, 2020, 1).returning(Seq(Payment(150.33, "yo", Category.entertainment)))
-          (mailerWrapper.sendEmail _).expects(where { (userId, subject, body) ⇒
-            userId == 1L && subject.contains("Unusual spending") && body.contains("Hello card user!")
-          })
+    "send an user notification when the user has unusual expenses" in new Setup {
+      private val unusualExpenses = Seq(UnusualExpense(10.00, Category.entertainment))
 
-          unusualSpendingEmail.trigger(1L)
-        }
-      }
+      (unusualExpensesAnalyzer.analyzeExpenses _).expects(userId).returning(unusualExpenses)
+      (unusualExpensesNotifier.notifyUser _).expects(userId, unusualExpenses).once
+
+      unusualSpendingEmail.trigger(userId)
     }
   }
-
 }
