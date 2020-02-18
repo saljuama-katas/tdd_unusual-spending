73179280b9bcf6a3fa513cfbdd04a4a9836825db
Salvador Juan Martinez <saljuama@gmail.com>
2020-02-18 01:31:11 +0100

    implement email notifier
    missing edge cases:
    - EmailsUser throwing errors
    - Deal with invalid arguments

---
+++ src/main/scala/spending/UnusualExpensesEmailNotifier.scala
@@ -0,0 +1,46 @@
+package spending
+
+
+class UnusualExpensesEmailNotifier(val emailsUserWrapper: EmailsUserWrapper) extends UnusualExpensesNotifier {
+
+  override def notifyUser(userId: Long, unusualExpenses: Seq[UnusualExpense]): Unit = {
+    emailsUserWrapper.email(userId, composeSubject(unusualExpenses), composeBody(unusualExpenses))
+  }
+
+  private def composeSubject(unusualExpenses: Seq[UnusualExpense]) = {
+    val totalUnusualExpenses = unusualExpenses.map { _.price }.sum
+    s"Unusual spending detected of ${formatCurrency(totalUnusualExpenses)} detected!"
+  }
+
+  private def composeBody(unusualExpenses: Seq[UnusualExpense]): String = {
+    val emailHeader =
+      """Hello card user!
+        |
+        |We have detected unusually high spending on your card in these categories:
+        |
+        |""".stripMargin
+    val emailFooter =
+      """
+        |
+        |Love,
+        |
+        |The Credit Card Company
+        |""".stripMargin
+    val unusualExpensesList = unusualExpenses
+      .map { expense ⇒ s"* You spent ${formatCurrency(expense.price)} on ${expense.category}" }
+      .mkString("\n")
+
+    emailHeader + unusualExpensesList + emailFooter
+  }
+
+  private def formatCurrency(amount: BigDecimal): String = {
+    val formatter = java.text.NumberFormat.getCurrencyInstance
+    formatter.format(amount)
+  }
+}
+
+trait EmailsUserWrapper {
+  def email(userId: Long, subject: String, body: String): Unit = {
+    EmailsUser.email(userId, subject, body)
+  }
+}
---
+++ src/test/scala/spending/UnusualExpensesEmailNotifierTest.scala
@@ -0,0 +1,47 @@
+package spending
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class UnusualExpensesEmailNotifierTest extends AnyWordSpec with Matchers with MockFactory {
+
+  trait Setup {
+    val emailsUserWrapper: EmailsUserWrapper = mock[EmailsUserWrapper]
+    val unusualExpensesEmailNotifier = new UnusualExpensesEmailNotifier(emailsUserWrapper)
+  }
+
+  "An email notifier" must {
+
+    "compose email addressed to the specified user" in new Setup {
+      emailsUserWrapper.email _ expects(1L, *, *)
+
+      unusualExpensesEmailNotifier.notifyUser(1L, Seq(
+        UnusualExpense(100.00, Category.entertainment),
+        UnusualExpense(50.00, Category.travel)
+      ))
+    }
+
+    "compose email subject with the sum of the expenses" in new Setup {
+      emailsUserWrapper.email _ expects where { (_, subject, _) ⇒ subject.contains("150.00") }
+
+      unusualExpensesEmailNotifier.notifyUser(1L, Seq(
+        UnusualExpense(100.00, Category.entertainment),
+        UnusualExpense(50.00, Category.travel)
+      ))
+    }
+
+    "compose email body with an entry for each unusual expense" in new Setup {
+      emailsUserWrapper.email _ expects where { (_, _, body) ⇒
+        body.contains("100.00 on entertainment") &&
+          body.contains("50.00 on travel")
+      }
+
+      unusualExpensesEmailNotifier.notifyUser(1L, Seq(
+        UnusualExpense(100.00, Category.entertainment),
+        UnusualExpense(50.00, Category.travel)
+      ))
+    }
+
+  }
+}
